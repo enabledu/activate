6aa0e3c4d920e16a2ac8cf4b63d12bca35b1b003
mabagoury <wnajsksos@gmail.com>
2023-02-11 12:50:57 +0200

    feat: add changes to the activate manager

--- activate/activate_manager.py
+++ activate/activate_manager.py
@@ -3,6 +3,10 @@ from typing import Callable
 from activate.types.activity_streams.core_types import Activity
 
 
+def get_activate_manager():
+    return ActivateManager()
+
+
 class ActivateManager:
     schema: dict[Activity, Callable]
 
@@ -10,10 +14,16 @@ class ActivateManager:
         """
         Called by the web adapter to resolve activities
         """
-        pass
+        # TODO: check the schema for a registered activity that matches the requested one
+        # TODO: if so, call it
 
     def resolver(self):
         """
         A decorator used by the package user to register custom resolvers for activities
         """
-        pass
+        def wrapper(resolver_function):
+            # TODO: check that the first argument to the function is an activity
+            # TODO: if so, register the resolver function in schema
+            return resolver_function
+
+        return wrapper
--- activate/web/fastapi/app.py
+++ activate/web/fastapi/app.py
@@ -1,16 +1,21 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, Body, Depends
 
-from activate.activate_manager import ActivateManager
+from activate.activate_manager import ActivateManager, get_activate_manager
 from activate.types.activity_streams.core_types import Activity
 
-app = FastAPI()
+activate_router = APIRouter()
 
 
-@app.get("/")
-async def activitypub_get_endpoint(activity: Activity, activate_manager:ActivateManager):
+# TODO: check the initialization of the activate manager
+# TODO: check the middleware to ensure the right content-type and accept header are correct
+
+@activate_router.get("/")
+async def activitypub_get_endpoint(activity: Activity = Body(),
+                                   activate_manager: ActivateManager = Depends(get_activate_manager)):
     return await activate_manager.resolve(activity)
 
 
-@app.post("/")
-async def activitypub_post_endpoint(activity: Activity, activate_manager:ActivateManager):
+@activate_router.post("/")
+async def activitypub_post_endpoint(activity: Activity,
+                                    activate_manager: ActivateManager = Depends(get_activate_manager)):
     return await activate_manager.resolve(activity)
